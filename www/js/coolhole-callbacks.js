26451c8a55e05034073c1eeb55e7a444c97bab37
streeeggs <forgotmycockli@protonmail.com>
2025-01-29 15:15:06 -0500

    fix: remove unuse function

--- www/js/coolhole-callbacks.js
+++ www/js/coolhole-callbacks.js
@@ -33,9 +33,6 @@ const CoolholeCallbacks = {
     // Update the UI
     // Check the users rank and fade in the counter/button
     initPointsForSelf(CLIENT.coolpoints);
-    if (CLIENT.rank >= 1) {
-      initCounterForSelf();
-    }
 
     // Init CoolPoints User List
     if (CLIENT.rank >= 2) {
@@ -66,5 +63,5 @@ const CoolholeCallbacks = {
   },
   coolpointsVoteskipFail: function (response) {
     $("#voteskip").attr("disabled", false);
-  }
+  },
 };
