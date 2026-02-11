fa2b818be122fa30b0fe311d64e283d61d19c2a0
streeeggs <forgotmycockli@protonmail.com>
2026-02-11 15:59:11 -0500

    fix(slots): bet input UI

--- www/js/coolhole-callbacks.js
+++ www/js/coolhole-callbacks.js
@@ -535,7 +535,6 @@ const CoolholeCallbacks = {
   },
 
   coolholeSpinSlotResponse: function (response) {
-    $("#cp-slots-spin-btn").prop("disabled", false);
     handleSlotSpinResponse(response);
   },
 };
