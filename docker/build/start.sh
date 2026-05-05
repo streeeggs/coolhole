b1cf78ec6e609353ef62e65d3087e21bd2a4e553
streeeggs <forgotmycockli@protonmail.com>
2026-05-04 21:34:26 -0400

    docker(vibe): what about now

--- docker/build/start.sh
+++ docker/build/start.sh
@@ -3,38 +3,14 @@ set -e
 
 cd /service/coolhole
 
-# ----- Runtime config generation -----
+echo "Starting Coolhole in $NODE_ENV mode"
+
 UUID=$(uuidgen)
 
-yq -i ".mysql.server = \"db\"" config.yaml
-yq -i ".mysql.port = \"${MARIADB_PORT}\"" config.yaml
-yq -i ".mysql.password = \"${CYTUBE_MARIADB_PASSWORD}\"" config.yaml
-yq -i ".http.root-domain = \"${COOLHOLE_URL}\"" config.yaml
-yq -i ".http.trust-proxies = [\"loopback\",\"uniquelocal\"]" config.yaml
-yq -i ".http.cookie-secret = \"${UUID}\"" config.yaml
-yq -i ".io.domain = \"http://${COOLHOLE_URL}\"" config.yaml
-yq -i ".ffmpeg.enabled = true" config.yaml
-yq -i ".youtube-v3-key = \"${YOUTUBE_V3_API_KEY}\"" config.yaml
-yq -i ".twitch-client-id = \"${TWITCH_CLIENT_ID}\"" config.yaml
-
-echo "Starting in $NODE_ENV mode"
+echo "Starting Node..."
 
 if [ "$NODE_ENV" = "development" ]; then
-    echo "Waiting for MariaDB to be ready..."
 
-    until mariadb -h db -u root -p"$MARIADB_ROOT_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
-        sleep 2
-    done
-
-    echo "MariaDB is ready."
-
-    # Start Babel in watch mode in the background
-    npm run server-dev &
-    BABEL_PID=$!
-
-    # Start server with inspector enabled, watching compiled lib/ for changes
-    # The delay ensures Babel finishes writing before nodemon restarts the process
-    exec nodemon --inspect=0.0.0.0:9229 --delay 1500 --watch lib index.js
 else
     exec forever index.js
 fi
